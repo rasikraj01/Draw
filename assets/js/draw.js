ee46a6b9f2086b42793575c42816ac4199b5403a
Rasik <rasikraj01@gmail.com>
2018-01-12 21:09:15 +0530

    updated draw.js

--- assets/js/draw.js
+++ assets/js/draw.js
@@ -1,9 +1,23 @@
-function setup() {
-   let c = createCanvas(400, 400);
-   c.position(0,0);
-   clear();
-}
+let s = function (sketch) {
 
-function draw() {
-   background(0);
-}
+   sketch.setup = function () {
+      document.body.style['userSelect'] = 'none';
+      let h = document.body.clientHeight;
+      let c = sketch.createCanvas(sketch.windowWidth, h);
+      c.position(0,0);
+      sketch.clear();
+
+      c.style('pointer-events', 'none');
+   }
+
+    sketch.draw = function () {
+      sketch.stroke(0);
+      sketch.strokeWeight(4);
+      if(sketch.mouseIsPressed){
+         sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+      }
+   }
+
+};
+
+let myp5 = new p5(s);
